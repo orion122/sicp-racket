a22cd6c84e4a0c912c8c350558cf9249a441cd14
ramazan <yaru@bk.ru>
2018-10-30 17:47:57 +0300

    edit 1.43: edit compose

--- sicp/chapter1/1.43.rkt
+++ sicp/chapter1/1.43.rkt
@@ -11,12 +11,12 @@
 (define (inc x)
   (+ x 1))
 
-(define (compose f1 f2 x)
-  (f1 (f2 x)))
+(define (compose f g)
+  (lambda (x) (f (g x))))
 
 (define (repited func times)
   (if (= times 1)
-      (lambda (x) (func x))
-      (lambda (x) (compose (lambda (x) (func x)) (repited func (- times 1)) x))))
+      func
+      (compose func (repited func (- times 1)))))
 
 (check-equal? ((repited square 2) 5) 625)
