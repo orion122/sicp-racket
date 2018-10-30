b6f808c6b8db5d3458e155f4360ef5863c8bbdc5
ramazan <yaru@bk.ru>
2018-10-30 17:31:38 +0300

    complete 1.43

---
+++ sicp/chapter1/1.43.rkt
@@ -0,0 +1,22 @@
+#lang racket/base
+
+(require rackunit)
+
+(define (square x)
+  (* x x))
+
+(define (cube x)
+  (* x x x))
+
+(define (inc x)
+  (+ x 1))
+
+(define (compose f1 f2 x)
+  (f1 (f2 x)))
+
+(define (repited func times)
+  (if (= times 1)
+      (lambda (x) (func x))
+      (lambda (x) (func ((repited func (- times 1)) x)))))
+
+(check-equal? ((repited square 2) 5) 625)
