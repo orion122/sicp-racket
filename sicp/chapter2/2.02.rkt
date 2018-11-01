44a0082b0c0fc3e798ec9b0213397484abb7824c
ramazan <yaru@bk.ru>
2018-11-01 14:26:26 +0300

    complete 2.02

---
+++ sicp/chapter2/2.02.rkt
@@ -0,0 +1,32 @@
+#lang racket/base
+
+(require rackunit)
+
+(define (make-segment a b) (cons a b))
+(define (start-segment segment) (car segment))
+(define (end-segment segment) (cdr segment))
+
+(define (make-point x y) (cons x y))
+(define (x-point point) (car point))
+(define (y-point point) (cdr point))
+
+(define (average x y) (/ (+ x y) 2))
+
+(define (midpoint-segment segment)
+  (make-point (average (x-point (start-segment segment))
+                       (x-point (end-segment segment)))
+              (average (y-point (start-segment segment))
+                       (y-point (end-segment segment)))))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
+
+(define test-segment (make-segment (make-point -2 -5) (make-point 2 3)))
+
+
+(check-equal? (print-point (midpoint-segment test-segment)) (cons 0 -1))
