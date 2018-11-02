961f4740e2972fd194e53bd1ff822750ad3ba444
ramazan <yaru@bk.ru>
2018-11-02 20:33:48 +0300

    complete 2.07

---
+++ sicp/chapter2/2.07.rkt
@@ -0,0 +1,16 @@
+#lang racket/base
+
+(require rackunit)
+
+(define (make-interval a b) (cons a b))
+
+(define (upper-bound interval)
+  (max (car interval)
+       (cdr interval)))
+
+(define (lower-bound interval)
+  (min (car interval)
+       (cdr interval)))
+
+(check-equal? (upper-bound (make-interval 7 2)) 7)
+(check-equal? (lower-bound (make-interval 7 2)) 2)
